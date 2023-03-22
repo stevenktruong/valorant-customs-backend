6b0684a45b44a82fc46c9bb3eada6e606e492f68
stevenktruong <steventruong@g.ucla.edu>
2023-03-22 03:06:09 -0700

    Disable caching of files sent

--- api/datasets.py
+++ api/datasets.py
@@ -2,8 +2,8 @@ from flask import app, send_file
 
 
 def dashboard():
-    return send_file("out-min/dashboard.json")
+    return send_file("out-min/dashboard.json", max_age=0)
 
 
 def wall_of_shame():
-    return send_file("out-min/wall-of-shame.json")
+    return send_file("out-min/wall-of-shame.json", max_age=0)
