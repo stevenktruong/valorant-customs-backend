e6d4dfb954775a78fc017bdf4ddfad035cf97fd3
stevenktruong <steventruong@g.ucla.edu>
2023-04-11 23:54:04 -0700

    Use a fixed Chrome version when scraping and update player info

--- process_scrape.py
+++ process_scrape.py
@@ -20,6 +20,7 @@ player_tags = {
     "spookslayer1": STEVE,
     "youngsmasher": STEVEN,
     "sun": SUN,
+    "sun emoji": SUN,
     "Susi sushiserver": SUSI,
     "SusTwins": SUSI,
     "danielscutiegf": SUSU,
@@ -39,11 +40,13 @@ player_ids = {
     "HdmC1fwo6fs0IR5aF8YPr_d8Y9O4mGvLEte-ZobPyIIGY_rgprMUsKVcU1bgNGrO64cY0mVe3wLyIA": LINDSEY,
     # "cc0hfNaF5_tny7jaSf8N7Bar87EQJt3fLsy85NKGFHBjixtmXfwYgZ9iaIqfaHSwyDvb6HbzOCCuCQ": SEQUENTIAL,
     "QUesI_kn1ehLlxurIHZtD9Ww-9qESBRQ_RodxkG7q_jWaVB5p7eh1VxnKUiuRmW_ABu8l2Elqr7MtQ": SOPHIE,
-    "g5ab9-AFyFHyaAzvd2E3YDPxyd_o7XMBQ3oMJEKvVcoCdD5DSWijihWgt4lO3RdeQ6glRVQVQZowTQ": STEVE,
-    "9AEt5kjyF3Ho7ZiXaaZA03mCw8_CCF4dP6J8QExrVssTB_5HDXaDhrsgwPzs_Y0o3QVxmN2iqoVpyA": STEVE,
+    "g5ab9-AFyFHyaAzvd2E3YDPxyd_o7XMBQ3oMJEKvVcoCdD5DSWijihWgt4lO3RdeQ6glRVQVQZowTQ": STEVE,  # Selintt
+    "9AEt5kjyF3Ho7ZiXaaZA03mCw8_CCF4dP6J8QExrVssTB_5HDXaDhrsgwPzs_Y0o3QVxmN2iqoVpyA": STEVE,  # alomeirca
     "ocm6y5CVE3L4l1dIcYoyKzZombwCuVx3vP7pqFe79wCdk_oCAUcaJ0mf7MGwK2lGMx4kt9xirfEq6Q": STEVEN,
-    "Ef0d2--EnEXjUkAjOCdoG8l-I6xhsTiwBYfO_hGiNc1ageb7ojel5_ih2GbI68EijY3i_wxgL0_E_A": SUN,
-    "QkVviF0Z7UsFptwr0fg-KEMlgbeL7Wg-WpcISvEKUc7CGkBrCt9e1t82Ma7wFwc_CL2Oi0R6Gn0-Fg": SUSI,
+    "Ef0d2--EnEXjUkAjOCdoG8l-I6xhsTiwBYfO_hGiNc1ageb7ojel5_ih2GbI68EijY3i_wxgL0_E_A": SUN,  # sun
+    "7c53ICB_o_VMrncGWc3mkMb6T0xS_1m79SFToMdrIGJYSYuccj03anR-tu1Leb8Nb_RQ4qI74hJUkg": SUN,  # sun emoji
+    "QkVviF0Z7UsFptwr0fg-KEMlgbeL7Wg-WpcISvEKUc7CGkBrCt9e1t82Ma7wFwc_CL2Oi0R6Gn0-Fg": SUSI,  # SusTwins
+    "io1l9zvfIf2_bH7FBZ0r6P3TJDWrNBisCwSN7NjyXAzasL-_W6FuhMGWfFNZZWDGRy8TI7g4hxw4hA": SUSI,  # Susi sushiserver
     "0JBuaLG15FiA7fEkE3ps24-anUziLtNoowIDmEV7ZBH5wu8gwojY3LKKYtAbKJM90UJY9Tmg7OhLwQ": SUSU,
     "LISiqRtWZpeNmJqirqfyBDFfgTMUNHFz-TuTmNqCGZ4OZSnHcx20SnJc1mDG28w40LHbkCO6ftm3YA": TANG,
     "fxBAnDFK0NgQ00iLw-G2hML2r4E3HAV11OIb-DTCMm77OHfSPogllCktnjup6AglEp2RrNNcgzWdYQ": YANG,
